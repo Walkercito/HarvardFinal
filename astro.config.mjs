e033e9b69e58548ec8b08a59d9c0216e923e93ca
Walkercito <yoanky2005@gmail.com>
2025-06-14 16:44:50 +0000

    rollback: clerk

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
+import clerk from "@clerk/astro";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [clerk()],
     vite: {
-        plugins: [tailwindcss()]
-    }
+      plugins: [tailwindcss()]
+  },
+  output: 'server',
+  adapter: vercel()
 });
